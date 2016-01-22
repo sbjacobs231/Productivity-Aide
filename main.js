e5123cebbe5839f10e1162a7cc6817681bb00148
Sky Jacobson <skyjacobson@skys-air.attlocal.net>
2016-01-21 17:30:22 -0800

    added a remove button

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function addBullet() {
 	//checkbox details
 	checkbox.setAttribute('type', 'checkbox')
 	checkbox.id = 'check'
+	checkbox.className = 'box'
+	li.className = 'txt'
 	//retrieve, package, display
 	var userInput = document.getElementById('userInput').value
 	var node = document.createTextNode(userInput)
@@ -12,4 +14,16 @@ function addBullet() {
 	li.appendChild(checkbox)
 	li.appendChild(node)
 	listItem.appendChild(li)
+}
+//remove function
+function removeBullet() {
+	var boxes = document.getElementsByClassName('box')
+	var txts = document.getElementsByClassName('txt')
+	for (i = 0; i<boxes.length; i++) {
+		var box = boxes[i]
+		var txt = txts[i]
+		if (box.checked) {
+			txt.parentNode.removeChild(txt)
+		}
+	}
 }
